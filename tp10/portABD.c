569f5ae5edde9390b4961741c64861f153498621
MateoLucas <mlucas@itba.edu.ar>
2020-05-19 14:53:06 -0300

    libreria 1.2

---
+++ tp10/portABD.c
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include <stdint.h>
+
+typedef union
+{
+    uint_fast8_t portA;
+    uint_fast8_t portB;
+    uint_fast16_t portD;
+}portABD;
+
