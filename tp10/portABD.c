4a1f64b72602e5a478687764f96881f090adf1d0
MateoLucas <mlucas@itba.edu.ar>
2020-05-19 16:32:20 -0300

    libreria 1.3

--- tp10/portABD.c
+++ tp10/portABD.c
@@ -4,11 +4,47 @@
  * and open the template in the editor.
  */
 #include <stdint.h>
+typedef struct
+{
+    uint_least8_t b0    :1;
+    uint_least8_t b1    :1;
+    uint_least8_t b2    :1;
+    uint_least8_t b3    :1;       
+    uint_least8_t b4    :1;
+    uint_least8_t b5    :1;
+    uint_least8_t b6    :1;
+    uint_least8_t b7    :1;
+}bit8_t;
+
+typedef struct
+{
+    uint_least8_t b0    :1;
+    uint_least8_t b1    :1;
+    uint_least8_t b2    :1;
+    uint_least8_t b3    :1;       
+    uint_least8_t b4    :1;
+    uint_least8_t b5    :1;
+    uint_least8_t b6    :1;
+    uint_least8_t b7    :1;
+    uint_least8_t b8    :1;
+    uint_least8_t b9    :1;
+    uint_least8_t b10   :1;
+    uint_least8_t b11   :1;       
+    uint_least8_t b12   :1;
+    uint_least8_t b13   :1;
+    uint_least8_t b14   :1;
+    uint_least8_t b15   :1;
+    
+}bit16_t;
 
 typedef union
 {
-    uint_fast8_t portA;
-    uint_fast8_t portB;
-    uint_fast16_t portD;
-}portABD;
+    bit8_t portA;
+    bit8_t portB;
+    bit16_t portD;
+}portABD_t;
 
+int bitSet(int n)
+{
+    
+}
