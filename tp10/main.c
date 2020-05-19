833ed05a95ae9a99af066916179a2b650b2cbab1
MateoLucas <mlucas@itba.edu.ar>
2020-05-19 14:51:34 -0300

    libreria 1.1

---
+++ tp10/main.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: mateo
+ *
+ * Created on May 19, 2020, 1:38 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/*
+ * Se pide escribir una librería que permita emular el funcionamiento de los puertos A, B
+y D. A y B son dos puertos de 8 bits, configurables tanto de entrada como de salida. D
+tiene 16 y es simplemente un alias para los puertos A y B juntos, siendo el B el menos
+significativo.
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
