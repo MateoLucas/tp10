471c15316f830af182b772fc9c7204036bd7974c
MateoLucas <mlucas@itba.edu.ar>
2020-05-23 15:32:59 -0300

    libreria 1.12

--- tp10/portABD.c
+++ tp10/portABD.c
@@ -11,40 +11,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-/*
-typedef struct
-{
-    uint_least8_t b0    :1;
-    uint_least8_t b1    :1;
-    uint_least8_t b2    :1;
-    uint_least8_t b3    :1;       
-    uint_least8_t b4    :1;
-    uint_least8_t b5    :1;
-    uint_least8_t b6    :1;
-    uint_least8_t b7    :1;
-}bit8_t;
-
-typedef struct
-{
-    uint_least16_t b0    :1;
-    uint_least16_t b1    :1;
-    uint_least16_t b2    :1;
-    uint_least16_t b3    :1;       
-    uint_least16_t b4    :1;
-    uint_least16_t b5    :1;
-    uint_least16_t b6    :1;
-    uint_least16_t b7    :1;
-    uint_least16_t b8    :1;
-    uint_least16_t b9    :1;
-    uint_least16_t b10   :1;
-    uint_least16_t b11   :1;       
-    uint_least16_t b12   :1;
-    uint_least16_t b13   :1;
-    uint_least16_t b14   :1;
-    uint_least16_t b15   :1;
-    
-}bit16_t;
-*/
+#define SIZE_ERROR(n,port) ((port=='a'&&n>7)||(port=='A'&&n>7)||(port=='b'&&n>7)||(port=='B'&&n>7)||(port=='d'&&n>15)||(port=='D'&&n>15))
 
 typedef union
 {
@@ -60,149 +27,184 @@ static portABD_t ports;
 /*****************************************************************************/
 int bitSet(int n,char port)
 {
-    int mask,ans = 0;void * pPort;
-    switch(port)
+    uint_least8_t mask = 0x1;
+    int ans = 0;
+    for(int i = 1;i<n;i++)
     {
-        case 'a':
-        case 'A':
-            (uint_least8_t*)pPort;
-            pPort= &(ports.portAB.portA);
-            (uint_least8_t)mask;
-            mask=0x1;break;
-        case 'b':
-        case 'B':
-            (uint_least8_t*)pPort;
-            pPort= &ports.portAB.portB;
-            (uint_least8_t)mask;
-            mask=0x1;break;
-        case 'd':
-        case 'D':
-            (uint_least16_t*)pPort;
-            pPort= &ports.portD;
-            (uint_least16_t)mask;
-            mask=0x1;break;
-        default: 
-            ans++;
+        mask<<=1;
     }
-    if(sizeof(*pPort)>n)
+    if(SIZE_ERROR(n,port))
     {
         ans++;
     }
     else
-    {
-        for(int i = 1;i<n;i++)
+    {    
+        switch(port)
         {
-        mask<<=1;
+            case 'a':
+            case 'A':
+                ports.portAB.portA|=mask;
+                break;
+            case 'b':
+            case 'B':
+                ports.portAB.portB|=mask;
+                break;
+            case 'd':
+            case 'D':
+            
+                (uint_least16_t)mask;
+                mask=0x01;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                }
+                ports.portD |= mask;
+                break;
+            default: 
+                ans++;
         }
-        (*pPort) = ((*pPort)|mask);
     }
     return ans;   
 }
 /******************************************************************************/
 int bitClr(int n,char port)
 {
-    int mask,ans = 0;int * pPort;
-    switch(port)
-    {
-        case 'a':
-        case 'A':
-            
-            (uint_least8_t*)*pPort = ports.portAB.portA;
-            (uint_least8_t)mask=0xE;break;
-        case 'b':
-        case 'B':
-            (uint_least8_t*)*pPort = ports.portAB.portB;
-            (uint_least8_t)mask=0xE;break;
-        case 'd':
-        case 'D':
-            (uint_least16_t*)*pPort = ports.portD;
-            (uint_least16_t)mask=0xFE;break;
-        default: 
-            ans++;
-    }
-    if(sizeof(*pPort)>n)
+    int mask;
+    int ans = 0;
+    if(SIZE_ERROR(n,port))
     {
         ans++;
     }
     else
     {
-        for(int i = 1;i<n;i++)
+        switch(port)
         {
-        mask<<=1;
-        mask+=1;
+            case 'a':
+            case 'A':
+                mask=0xE;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                    mask +=1;
+                }
+                ports.portAB.portA=(ports.portAB.portA|mask);
+                break;
+            case 'b':
+            case 'B':
+                mask=0xE;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                    mask +=1;
+                }
+                ports.portAB.portB|=mask;
+                break;
+            case 'd':
+            case 'D':
+            
+                (uint_least16_t)mask;
+                mask=0xFE;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                    mask +=1;
+                }
+                ports.portD &= mask;
+                break;
+            default: 
+                ans++;
         }
-        (*pPort) = (*pPort)&mask;
     }
-    return ans;   
+    return ans;  
 }
 /*****************************************************************************/
 int bitGet(int n,char port)
 {
-    int mask,ans = 0;int * pPort;
-    switch(port)
+    uint_least8_t mask=0x1,ans = 0;
+    for(int i = 1;i<n;i++)//acomodo la mascara
     {
-        
-        case 'a':
-        case 'A':
-            (uint_least8_t*)*pPort = ports.portAB.portA;
-            (uint_least8_t)mask=0x1;break;
-        case 'b':
-        case 'B':
-            (uint_least8_t*)*pPort = ports.portAB.portB;
-            (uint_least8_t)mask=0x1;break;
-        case 'd':
-        case 'D':
-            (uint_least16_t*)*pPort = ports.portD;
-            (uint_least16_t)mask=0x1;break;
-        default: 
-            ans--;
+        mask<<=1;
     }
-    if(sizeof(*pPort)>n)//me aseguro que el numero de bit este contenido en el QUE PASA SI sizeof(void*pPort) 
+   
+    
+    if(SIZE_ERROR(n,port))//me aseguro que el numero de bit este contenido en el QUE PASA SI sizeof(void*pPort) 
     {                               //puerto.
         ans--;
     }
     else
     {
-        for(int i = 1;i<n;i++)//acomodo la mascara
+         switch(port)
         {
-        mask<<=1;
+            case 'a':
+            case 'A':
+                ans = (ports.portAB.portA&mask);
+                break;
+            case 'b':
+            case 'B':
+                ans=(ports.portAB.portB&mask);
+                break;
+            case 'd':
+            case 'D':
+                (uint_least16_t)mask;
+                mask=0x01;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                }
+                ans = ports.portD&mask;
+                break;
+            default: 
+                ans--;
+            
         }
-        ans = (*pPort)&mask;
-    }
     return ans; //ans<0 -> error | ans>0 -> encendido | ans == 0 -> apagado.
+    }
 }
 /******************************************************************************/
 int bitToggle(int n,char port)
 {
-    int mask,ans = 0;int * pPort;
-    switch(port)
-    {
-        case 'a':
-        case 'A':
-            (uint_least8_t*)*pPort = ports.portAB.portA;
-            (uint_least8_t)mask=0x1;break;
-        case 'b':
-        case 'B':
-            (uint_least8_t*)*pPort = ports.portAB.portB;
-            (uint_least8_t)mask=0x1;break;
-        case 'd':
-        case 'D':
-            (uint_least16_t*)*pPort = ports.portD;
-            (uint_least16_t)mask=0x1;break;
-        default: 
-            ans++;
-    }  
-    if(sizeof(*pPort)>n)
+    int mask;
+    int ans = 0;
+    if(SIZE_ERROR(n,port))
     {
         ans++;
     }
-    if(ans == 0)
+    else
     {
-        for(int i = 1;i<n;i++)
+        switch(port)
         {
-        mask<<=1;
+            case 'a':
+            case 'A':
+                mask=0xE;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                }
+                ports.portAB.portA=(ports.portAB.portA^mask);
+                break;
+            case 'b':
+            case 'B':
+                mask=0xE;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                }
+                ports.portAB.portB^=mask;
+                break;
+            case 'd':
+            case 'D':
+            
+                (uint_least16_t)mask;
+                mask=0xFE;
+                for(int i = 1;i<n;i++)
+                {
+                    mask<<=1;
+                }
+                ports.portD ^= mask;
+                break;
+            default: 
+                ans++;
         }
-        (*pPort)= (*pPort)^mask;
     }
-    return ans;   
+    return ans;
 }
